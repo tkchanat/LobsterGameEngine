0da5a5fe8d34e926d7da3ba206dcef7b3acb070d
tkchanat <andychancse@gmail.com>
2019-11-01 10:27:54 +0800

    PBR done... for now?

--- LobsterGameEngine/res/shaders/PBR.glsl
+++ LobsterGameEngine/res/shaders/PBR.glsl
@@ -51,41 +51,29 @@ uniform sampler2D RoughnessMap;
 uniform sampler2D MetallicMap;
 uniform sampler2D AmbientOcclusionMap;
 
-const float PI = 3.14159265359;
+#define PI 3.14159265359
+#define LIGHT_COUNT 1
 
-float Distribution(vec3 n, vec3 h, float roughness)
+float Distribution(float cosLh, float roughness)
 {
 	float a = roughness * roughness;
     float a2 = a * a;
-    float nh = max(dot(n, h), 0.0);
-    float nh2 = nh * nh;
-	
-    float nom = a2;
+    float nh2 = cosLh * cosLh;
     float denom = (nh2 * (a2 - 1.0) + 1.0);
-    denom = PI * denom * denom;
 	
-    return nom / max(denom, 0.001);
+    return a2 / max(PI * denom * denom, 0.001);
 }
 
-float SchlickGGX(float nv, float roughness)
+float SchlickGGX(float cosTheta, float k)
 {
-    float r = (roughness + 1.0);
-    float k = (r*r) / 8.0;
-
-    float nom = nv;
-    float denom = nv * (1.0 - k) + k;
-	
-    return nom / max(denom, 0.001);
+    return cosTheta / (cosTheta * (1.0 - k) + k);
 }
   
-float Geometry(vec3 n, vec3 v, vec3 l, float roughness)
+float Geometry(float cosLi, float nv, float roughness)
 {
-    float nv = max(dot(n, v), 0.0);
-    float nl = max(dot(n, l), 0.0);
-    float ggx1 = SchlickGGX(nv, roughness);
-    float ggx2 = SchlickGGX(nl, roughness);
-	
-    return ggx1 * ggx2;
+    float r = roughness + 1.0;
+	float k = (r * r) / 8.0;
+    return SchlickGGX(cosLi, k) * SchlickGGX(nv, k);
 }
 
 vec3 Fresnel(float cosTheta, vec3 f0)
@@ -96,50 +84,44 @@ vec3 Fresnel(float cosTheta, vec3 f0)
 void main()
 {
 	// texture maps
-	vec3 albedo = mix(vec3(1.0), texture(AlbedoMap, frag_texcoord).rgb, float(PBR.UseAlbedoMap));
+	vec3 albedo = float(PBR.UseAlbedoMap) > 0.5 ? texture(AlbedoMap, frag_texcoord).rgb : PBR.Albedo;
 
 	vec3 n = normalize(frag_normal);
 	vec3 v = normalize(cameraPosition - frag_position);
+	vec3 l = normalize(lightPosition);
+	vec3 h = normalize(v + l);
+	float cosLi = max(dot(n, l), 0.0);
+	float cosLh = max(dot(n, h), 0.0);
+	float nv = max(dot(n, v), 0.0);
 
 	// choose initial reflectance value at normal incidence
-	vec3 f0 = vec3(0.04);
-	f0 = mix(f0, albedo, PBR.Metallic);
+	vec3 f0 = mix(vec3(0.04), albedo, PBR.Metallic);
 
 	// reflectance equation
 	vec3 Lo = vec3(0.0);
-	// for(int i = 0; i < 1; ++i)
-	// {
+	for(int i = 0; i < LIGHT_COUNT; ++i)
+	{
 		// Radiance
-		vec3 l = normalize(lightPosition);
-		vec3 h = normalize(v + l);
 		float distance = length(lightPosition - frag_position);
 		float attenuation = 1.0 / (distance * distance);
-		vec3 radiance = lightColor * attenuation;
+		vec3 radiance = lightColor;// * attenuation;
 
 		// BRDF
-		float D = Distribution(n, h, PBR.Roughness);
-		float G = Geometry(n, v, l, PBR.Roughness);
 		vec3 F = Fresnel(clamp(dot(h, v), 0.0, 1.0), f0);
-		vec3 nom = D * G * F;
-		float denom = max(dot(n, v), 0.0) * max(dot(n, l), 0.0);
-		vec3 specular = nom / max(denom, 0.001);
+		float D = Distribution(cosLh, PBR.Roughness);
+		float G = Geometry(cosLi, nv, PBR.Roughness);
+		vec3 specular = (F * D * G) / max(4.0 * nv * cosLi, 0.001);
 
 		// Energy conservation
 		vec3 kS = F;
-		vec3 kD = vec3(1.0) - kS;
-		kD *= 1.0 - PBR.Metallic;
+		vec3 kD = (1.0 - F) * (1.0 - PBR.Metallic);
+		vec3 diffuse = kD * albedo;
 		
-		float intensity = max(dot(n, l), 0.0);
-		Lo += (kD * albedo / PI + specular) * radiance * intensity;
-	// }
+		Lo += (diffuse + specular) * radiance * cosLi;
+	}
 
-	vec3 ambient = PBR.Albedo * PBR.AmbientOcclusion * albedo;
-	vec3 color = ambient + Lo;
+	// vec3 ambient = PBR.Albedo * PBR.AmbientOcclusion * albedo;
+	// vec3 color = ambient + Lo;
 
-	// HDR tonemapping
-    // color = color / (color + vec3(1.0));
-    // gamma correct
-    // color = pow(color, vec3(1.0/2.2)); 
-
-	out_color = vec4(color, 1.0);
+	out_color = vec4(Lo, 1.0);
 } 
